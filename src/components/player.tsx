06dd22c871ab126de99e3a4ffb0199e55da8e2dc
Oscar Jansson <oscja313@student.liu.se>
2025-02-14 21:04:40 +0100

    started player

---
+++ src/components/player.tsx
@@ -0,0 +1,14 @@
+type Vec2 = {
+  x: number,
+  y: number
+}
+export default function Player(props: {
+  id: number,
+  gridDimention: number,
+  position: Vec2,
+  color: string
+}) {
+  return (
+    <svg
+  )
+}
