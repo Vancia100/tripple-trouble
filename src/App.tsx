1cc0ff23ee8cf98491101a763d60aa3733d9787d
Vancia100 <vilhelm.bergqvist@gmail.com>
2025-02-16 14:46:15 +0100

    Deployment setup

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,7 @@ import BaseDiv from './base_div'
 const pages:Record<string, (props: {
   setPage: Dispatch<React.SetStateAction<keyof typeof pages>>
 }) => ReactNode> = {
-  "Menu": (props) => { return <div><h1>Menu</h1> <button onClick={() => props.setPage("Game")}>{"Click to play"}</button></div>},
+  "Menu": (props) => { return <div><h1>Tripple Trouble</h1> <button onClick={() => props.setPage("Game")}>{"Click to play"}</button></div>},
   "Game": (props) => <BaseDiv {...props} />,
 } as const
 
